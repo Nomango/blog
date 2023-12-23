724a6f0a626c993288947075eaf46dd04e22593d
Haibo <nomango@qq.com>
2023-12-23 12:04:06 +0800

    chore: use local fonts

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,44 +1,74 @@
+import fs from "node:fs/promises";
 import satori, { type SatoriOptions, type Font } from "satori";
 import { Resvg } from "@resvg/resvg-js";
 import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
 import type { Post } from "./getPosts";
 
-const fetchFonts = async (): Promise<Font[]> => {
-  const fetchFont = async (url: string, font: Omit<Font, "data">): Promise<Font> => {
-    const fontFile = await fetch(url);
-    const data: ArrayBuffer = await fontFile.arrayBuffer();
-    return {
-      ...font,
-      data: data,
-    };
+async function remoteFont(url: string, font: Omit<Font, "data">): Promise<Font> {
+  const fontFile = await fetch(url);
+  const data: ArrayBuffer = await fontFile.arrayBuffer();
+  return {
+    ...font,
+    data: data,
   };
+}
 
+async function localFont(path: string, font: Omit<Font, "data">): Promise<Font> {
+  const url = new URL(path, import.meta.url);
+  const data = await fs.readFile(url);
+  return {
+    ...font,
+    data: data,
+  };
+}
+
+async function collectFonts(): Promise<Font[]> {
   return Promise.all([
-    fetchFont("https://cdn.jsdelivr.net/fontsource/fonts/ibm-plex-mono@latest/latin-400-normal.ttf", {
+    localFont("../assets/fonts/ibm-plex-mono_5.0.8_latin-400-normal.ttf", {
       name: "IBM Plex Mono",
       weight: 400,
       style: "normal",
     }),
-    fetchFont("https://cdn.jsdelivr.net/fontsource/fonts/ibm-plex-mono@latest/latin-600-normal.ttf", {
+    localFont("../assets/fonts/ibm-plex-mono_5.0.8_latin-600-normal.ttf", {
       name: "IBM Plex Mono",
       weight: 600,
       style: "normal",
     }),
-    fetchFont("https://cdn.jsdelivr.net/fontsource/fonts/noto-sans-sc@latest/chinese-simplified-400-normal.woff", {
+    localFont("../assets/fonts/noto-sans-sc_5.0.17_chinese-simplified-400-normal.woff", {
       name: "Noto Sans SC",
       weight: 400,
       style: "normal",
     }),
-    fetchFont("https://cdn.jsdelivr.net/fontsource/fonts/noto-sans-sc@latest/chinese-simplified-600-normal.woff", {
+    localFont("../assets/fonts/noto-sans-sc_5.0.17_chinese-simplified-600-normal.woff", {
       name: "Noto Sans SC",
       weight: 600,
       style: "normal",
     }),
+    // remoteFont("https://cdn.jsdelivr.net/fontsource/fonts/ibm-plex-mono@latest/latin-400-normal.ttf", {
+    //   name: "IBM Plex Mono",
+    //   weight: 400,
+    //   style: "normal",
+    // }),
+    // remoteFont("https://cdn.jsdelivr.net/fontsource/fonts/ibm-plex-mono@latest/latin-600-normal.ttf", {
+    //   name: "IBM Plex Mono",
+    //   weight: 600,
+    //   style: "normal",
+    // }),
+    // remoteFont("https://cdn.jsdelivr.net/fontsource/fonts/noto-sans-sc@latest/chinese-simplified-400-normal.woff", {
+    //   name: "Noto Sans SC",
+    //   weight: 400,
+    //   style: "normal",
+    // }),
+    // remoteFont("https://cdn.jsdelivr.net/fontsource/fonts/noto-sans-sc@latest/chinese-simplified-600-normal.woff", {
+    //   name: "Noto Sans SC",
+    //   weight: 600,
+    //   style: "normal",
+    // }),
   ]);
-};
+}
 
-const fonts = await fetchFonts();
+const fonts = await collectFonts();
 
 const options: SatoriOptions = {
   width: 1200,
